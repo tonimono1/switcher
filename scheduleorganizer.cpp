3ed983403cfd29f4c9d35b93b589435b7d4e412a
Tobias Schauerte <tobias.schauerte@trilux.com>
2020-12-14 15:39:06 +0100

    Switch gpio 21 at sunrise and sunset

--- scheduleorganizer.cpp
+++ scheduleorganizer.cpp
@@ -1,63 +1,77 @@
 #include "scheduleorganizer.h"
 #include "astrocalculator.h"
 #include <QDebug>
+#include <QProcess>
 #include <math.h>
 
-ScheduleOrganizer::ScheduleOrganizer(QObject *parent) : QObject(parent)
+ScheduleOrganizer::ScheduleOrganizer(QObject *parent) : QObject(parent) 
 {
-    connect(&updateTimer, &QTimer::timeout, this, &ScheduleOrganizer::calculateNewEvents);
-    connect(&sunsetEventTimer, &QTimer::timeout, this, &ScheduleOrganizer::sunsetEvent);
-    connect(&sunriseEventTimer, &QTimer::timeout, this, &ScheduleOrganizer::sunriseEvent);
-    calculateNewEvents();
+  connect(&updateTimer, &QTimer::timeout, this,
+          &ScheduleOrganizer::calculateNewEvents);
+  connect(&sunsetEventTimer, &QTimer::timeout, this,
+          &ScheduleOrganizer::sunsetEvent);
+  connect(&sunriseEventTimer, &QTimer::timeout, this,
+          &ScheduleOrganizer::sunriseEvent);
+  calculateNewEvents();
 }
 
-void ScheduleOrganizer::calculateNewEvents()
+void ScheduleOrganizer::calculateNewEvents() 
 {
-    QDate currentDate = QDate::currentDate();
+  QDate currentDate = QDate::currentDate();
 
-    double sunrise = AstroCalculator::calculateSunrise(51.370787, 7.929925, currentDate.dayOfYear(), 0);
-    double sunset = AstroCalculator::calculateSunset(51.370787, 7.929925, currentDate.dayOfYear(), 0);
+  double sunrise = AstroCalculator::calculateSunrise(
+      51.370787, 7.929925, currentDate.dayOfYear(), 0);
+  double sunset = AstroCalculator::calculateSunset(51.370787, 7.929925,
+                                                   currentDate.dayOfYear(), 0);
 
-    QTime sunriseTime(static_cast<int>(sunrise),   static_cast<int>(std::round(std::fmod(sunrise, 1) * 60)));
-    QTime sunsetTime(static_cast<int>(sunset),   static_cast<int>(std::round(std::fmod(sunset, 1) * 60)));
-    scheduleSunriseEvent(sunriseTime);
-    scheduleSunsetEvent(sunsetTime);
-    updateTimer.stop();
-    updateTimer.start(milliSecondsTo(QTime(0, 0)));
+  QTime sunriseTime(static_cast<int>(sunrise),
+                    static_cast<int>(std::round(std::fmod(sunrise, 1) * 60)));
+  QTime sunsetTime(static_cast<int>(sunset),
+                   static_cast<int>(std::round(std::fmod(sunset, 1) * 60)));
+  scheduleSunriseEvent(sunriseTime);
+  scheduleSunsetEvent(sunsetTime);
+  updateTimer.stop();
+  updateTimer.start(milliSecondsTo(QTime(0, 0)));
 }
 
-void ScheduleOrganizer::sunsetEvent()
+void ScheduleOrganizer::sunsetEvent() 
 {
-    qDebug() << QStringLiteral("Sunset event at %1").arg(QDateTime::currentDateTimeUtc().toString());
+  qDebug() << QStringLiteral("Sunset event at %1")
+                  .arg(QDateTime::currentDateTimeUtc().toString());
 }
 
-void ScheduleOrganizer::sunriseEvent()
+void ScheduleOrganizer::sunriseEvent() 
 {
-    qDebug() << QStringLiteral("Sunrise event at %1").arg(QDateTime::currentDateTimeUtc().toString());
+  qDebug() << QStringLiteral("Sunrise event at %1")
+                  .arg(QDateTime::currentDateTimeUtc().toString());
 }
 
-int ScheduleOrganizer::milliSecondsTo(const QTime &toTime)
+int ScheduleOrganizer::milliSecondsTo(const QTime &toTime) 
 {
-    constexpr int MILLI_SECONDS_PER_DAY = 24 * 60 * 60 * 1000;
+  constexpr int MILLI_SECONDS_PER_DAY = 24 * 60 * 60 * 1000;
 
-    auto currentTime = QDateTime::currentDateTimeUtc().time();
-    int           milliSeconds          = currentTime.msecsTo(toTime);
-    if (milliSeconds < 0) {
-        milliSeconds += MILLI_SECONDS_PER_DAY;
-    }
-    return milliSeconds;
+  auto currentTime = QDateTime::currentDateTimeUtc().time();
+  int milliSeconds = currentTime.msecsTo(toTime);
+  if (milliSeconds < 0) {
+    milliSeconds += MILLI_SECONDS_PER_DAY;
+  }
+  return milliSeconds;
 }
 
-void ScheduleOrganizer::scheduleSunriseEvent(const QTime &atTime)
+void ScheduleOrganizer::scheduleSunriseEvent(const QTime &atTime) 
 {
-    sunriseEventTimer.stop();
-    sunriseEventTimer.start(milliSecondsTo(atTime));
-    qDebug() << QStringLiteral("Next Sunrise Event at: %1").arg(atTime.toString());
+  sunriseEventTimer.stop();
+  sunriseEventTimer.start(milliSecondsTo(atTime));
+  qDebug()
+      << QStringLiteral("Next Sunrise Event at: %1").arg(atTime.toString());
+
+  QProcess::execute("sh -c", {"echo 1 > /sys/class/gpio/gpio21/value"});
 }
 
-void ScheduleOrganizer::scheduleSunsetEvent(const QTime &atTime)
+void ScheduleOrganizer::scheduleSunsetEvent(const QTime &atTime) 
 {
-    sunsetEventTimer.stop();
-    sunsetEventTimer.start(milliSecondsTo(atTime));
-    qDebug() << QStringLiteral("Next Sunset Event at: %1").arg(atTime.toString());
+  sunsetEventTimer.stop();
+  sunsetEventTimer.start(milliSecondsTo(atTime));
+  qDebug() << QStringLiteral("Next Sunset Event at: %1").arg(atTime.toString());
+  QProcess::execute("sh -c", {"echo 0 > /sys/class/gpio/gpio21/value"});
 }
